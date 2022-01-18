c3690270ef1c14b168f28bc0c3d60566529d023c
Kelly Yun <kellyyun@hks.harvard.edu>
2022-01-17 23:29:26 -0500

    filtering task

--- filter.py
+++ filter.py
@@ -19,16 +19,3 @@ for row in reader:
     	# write rows that match above filter
         writer.writerow(row)
 
-#4 make the script executable (chmod +x filter.py)
-chmod +x /Development/assignments/filter.py
-
-#5 curl the 2017 quarter 1 expenditure file from
-curl -N "https://projects.propublica.org/congress/assets/staffers/2017Q1-house-disburse-detail.csv" | ./filter.py
-
-#6 redirect the output into a file expensive_water.csv
-command > expensive_water.csv
-
-#7 pipe expensive_water.csv into csvstat and redirect that into a file called expensive_water_summary.txt
-expensive_water.csv | csvstat
-
-command > expensive_water_summary.txt
