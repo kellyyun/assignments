95ff4bb10ddd986658eb42a58eca719d49e533db
Kelly Yun <kellyyun@hks.harvard.edu>
2022-01-17 22:03:17 -0500

    calculator.js

---
+++ calculator.js
@@ -0,0 +1,19 @@
+
+// This part of the code defines a function
+function multiply(a,b) { 
+    return a * b
+}
+
+function divide(a,b) { 
+    return a / b
+}
+
+function add(a,b) { 
+    return a + b
+}
+
+function subtract(a,b) { 
+    return a - b
+}
+
+
